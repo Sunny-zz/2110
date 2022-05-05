474f22bc980f292435cce502e0ff84f5abd4ce7f
Sunny-zz <b695340402@qq.com>
2022-05-05 20:29:38 +0800

    0505 end

--- vue_lesson/vue-hello1/src/App.vue
+++ vue_lesson/vue-hello1/src/App.vue
@@ -2,26 +2,37 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <img src="./assets/1.jpg" alt="">
-    <FirstComponent />
-    <FirstComponent />
-    <FirstComponent />
-    <FirstComponent />
+    <img src="./assets/11.jpg" alt="">
     <FirstComponent />
     <div class="box"></div>
+    <hr>
+    <GoodsItem title='生鲜馆' secondTitle='尝遍天下鲜' picOne='https://img13.360buyimg.com/img/s100x100_jfs/t1/177052/17/11003/120319/60d4099aEe6c40bef/0a1ea8aec41c528c.jpg!cc_100x100.webp' picTwo='https://img12.360buyimg.com/img/s100x100_jfs/t1/150926/15/4469/167602/5f977d54Ee011e70f/686c2f3b3a4d99d5.jpg!cc_100x100.webp' />
+    <GoodsItem title='Joy' secondTitle='Joy' :picOne='picOne' :picTwo='picTwo' />
+    <!-- html 中没有数字的概念，所以 template 中要使用数字的话需要使用模版语法 -->
+    <GoodsItem :secondTitle='10' :picOne='picOne' :picTwo='picTwo' />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import FirstComponent from './components/FirstComponent.vue'
+import GoodsItem from './components/GoodsItem.vue'
+import picOne from './assets/1.jpg'
+import picTwo from './assets/2.jpg'
 
 export default {
   name: 'App',
   components:{
     HelloWorld,
-    FirstComponent
-  } 
+    FirstComponent,
+    GoodsItem
+  },
+  data(){
+    return {
+      picOne: picOne,
+      picTwo: picTwo
+    }
+  }
 }
 </script>
 
@@ -30,14 +41,15 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
 .box  {
   width: 200px;
   height: 200px;
-  background-image: url('./assets/1.jpg');
+  background-image: url('./assets/11.jpg');
 }
 </style>
+
 
---
+++ vue_lesson/vue-hello1/src/components/GoodsItem.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="goods-item">
+    <h3>{{title}}<span>{{secondTitle}}</span></h3>
+    <div class="show-pic">
+      <img :src="picOne"   alt="">
+      <img :src="picTwo" alt="">
+    </div>
+  </div>
+</template>
+
+<script> 
+// 子组件接收父组件传递的 prop
+// 在导出的对象下写个 props 属性，该属性值是数组, 数组中写属性名字符串，就可以接收父组件传递过来的对应的属性
+// 这个写 prop 属性可以直接在 template 中使用，但是使用的时候需要了解 vue 的模版语法
+
+export default {
+  // props: ['title', 'secondTitle','picOne','picTwo']
+  // props 接收的高级写法
+  // 将 props 写成对象类型， 对象下的每一个属性就是接收的 prop , 主要为了 对 prop 进行校验
+  // 可以参考官方文档 prop --->  prop 验证
+  // 属性值有两种方式
+  // 1. 直接写值的类型 例如 字符串类型  String  意思就是传递的数据必须是 字符串
+  // props:{
+  //   title: String,
+  //   secondTitle: String,
+  //   picOne: String,
+  //   picTwo: String
+  // }
+  // 2. 直接写成对象 对象下可以添加 require  type   default 属性
+  props:{
+    title: {
+      // 是否必须传递   默认是 false 
+      // require: true,
+      // 值的类型
+      type: String,
+      // 默认值 和 require 不一起使用
+      default: '手机1',
+      // 自定义校验函数
+      // value 就是传递的值
+      validator(value){
+        // 需要return 布尔值
+        return value.length > 2
+      }
+    },
+    secondTitle: String,
+    picOne: String,
+    picTwo: String
+  }
+}
+</script>
+
+<style>
+  .goods-item {
+    width: 290px;
+    height: 180px;
+    margin-right: 10px;
+    margin-left: 10px;
+    padding: 16px 30px 0px;
+    box-shadow: 1px 1px 3px #ccc;
+    display: inline-block;
+  }
+  .goods-item h3 {
+    margin: 0;
+    margin-bottom: 20px;
+  }
+  .goods-item h3 span {
+    font-size: 14px;
+    color: #ccc;
+  }
+  .goods-item img:first-child{
+    margin-right: 30px;
+  }
+  
+</style>
