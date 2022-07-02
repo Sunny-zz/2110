2d84c4a4ae95cb4c3ee00b09bea7c431fa9f5b01
Sunny-zz <b695340402@qq.com>
2022-07-02 11:55:28 +0800

    0702 a

--- vue_lesson/vue-echarts-demo/src/App.vue
+++ vue_lesson/vue-echarts-demo/src/App.vue
@@ -4,6 +4,10 @@
     <BarDemo />
     <hr>
     <PieDemo />
+    <hr>
+    <p>{{num}}</p>
+    <button @click="num = {value : 100}">修改 num 第一次</button>
+    <button @click="num.value = 200">修改 num 第二次</button>
   </div>
 </template>
 
@@ -12,7 +16,11 @@ import BarDemo from './components/BarDemo.vue'
 import PieDemo from './components/PieDemo.vue'
 export default {
   components: { BarDemo, PieDemo },
-
+  data() {
+    return {
+      num: 100
+    }
+  },
 }
 </script>
 
